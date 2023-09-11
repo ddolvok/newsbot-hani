9d040c98c05adee855ca1e42fdfc1e86aaf87679
ddolvok <133072176+ddolvok@users.noreply.github.com>
2023-09-11 10:02:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,15 @@ from bs4 import BeautifulSoup
 import re
 import time
 import json
+import uuid
+
+if 'session_id' not in st.session_state:
+    st.session_state.session_id = str(uuid.uuid4())
 
 if 'summarized_content' not in st.session_state:
     st.session_state.summarized_content = ""
-if 'final_article_content' not in st.session_state:
-    st.session_state.final_article_content = ""
-if 'prompt' not in st.session_state:
-    st.session_state.prompt = ""
 
 API_KEY = st.secrets["api_key"]
-
-# Constants
 MAX_RETRY = 10
 WAIT_TIME = 5
 MAX_ARTICLE_SIZE = 2500
@@ -44,10 +42,11 @@ def fetch_from_openai(model, messages, spinner_text):
             else:
                 st.error(f"Error: {response.status_code}, {response.json()}")
                 return None
-        st.error(f"{MAX_RETRY}번 시도하고 실패함. 잠시 후 다시 해보세요.")
-        return None
+    st.error(f"{MAX_RETRY}번 시도하고 실패함. 잠시 후 다시 해보세요.")
+    return None
 
-def crawl_and_get_article(url, index):
+def crawl_and_get_article(url, index, session_id):
+    crawled_article = {}
     for _ in range(MAX_RETRY):
         r = requests.get(url)
         if r.status_code == 200:
@@ -68,11 +67,16 @@ def crawl_and_get_article(url, index):
     if len(article_text) > MAX_ARTICLE_SIZE:
         return None
 
-    return {"title": title_text, "content": article_text}
+    crawled_article = {"title": title_text, "content": article_text}
+
+    with open(f'crawled_article_{session_id}_{index}.json', 'w') as f:
+        json.dump(crawled_article, f)
+
+    return crawled_article
 
 def main():
-    st.title("미디어랩 뉴스봇 보고봇 PRJ")
-    
+    st.title("미디어랩 뉴스봇 보고봇 Project")
+
     keyword1 = st.text_input("1번 검색어 : ")
     keyword2 = st.text_input("2번 검색어 : ")
     keyword3 = st.text_input("3번 검색어 : ")
@@ -89,15 +93,17 @@ def main():
             return
 
         summarized_content = ""
+
         crawled_count = 0
         for index, link in enumerate(naver_news_links):
             if crawled_count >= 3:
                 break
-            crawled_article = crawl_and_get_article(link, index + 1)
+            crawled_article = crawl_and_get_article(link, index + 1, st.session_state.session_id)
             if crawled_article is None:
                 continue
 
             crawled_count += 1
+
             spinner_text = [
                 "첫 기사를 GPT4가 정리 하고 있습니다.",
                 "다음 기사를 GPT4가 정리하고 있습니다.",
@@ -105,39 +111,27 @@ def main():
             ][crawled_count - 1]
             summarized_content += fetch_from_openai("gpt-4", [
                 {"role": "user",
-                 "content": f"{crawled_article['title']} 및 {crawled_article['content']} 내용들을 잘 정리해서 기사 스타일로 쓰여진 보고 자료를 만들어. 기사들에서 다루는 공통된 내용은 하나로 합쳐서 정리해주고, 이어서 크롤링 되어 분석하는 기사의 내용이 직전 내용과 다른 세부 내용을 다루는 경우 줄을 바꿔서 정리본을 다시 시작해. 같은 내용을 다루더라도 다른 기사의 내용은 줄 바꿔서 다시 시작해. 숫자는 정확히 나오도록 정리해. '눈길을 끌었다' '주목된다' 등 판단이나 창의적인 표현들은 빼고 2500자 이내로 써 줘. 문장의 마무리는 늘 '했다' '됐다' '줬다' '였다' 처럼 끝내야 해. 내용 중에 [] 이 대괄호나 = 같은 부호가 들어가지 않게 해줘."}
+                 "content": f"{crawled_article['title']} 및 {crawled_article['content']} 내용들을 잘 정리해서 신문 기사 스타일의 보고 자료를 만들어. 다루는 공통된 내용과 공통되지 않은 내용 모두 포함해 전체 내용이 잘 드러나는 기사 스타일의 보고 자료로 만들거야. 키워드, 숫자 등을 잘 확인해. '눈길을 끌었다' '주목된다' 등 판단이나 창의적인 내용들은 빼고 2500자 이내로 써 줘."}
             ], spinner_text)
-        
-        st.session_state.summarized_content = summarized_content
 
-    if st.session_state.summarized_content:
-        st.write("## 참고용 리포트")
+        st.session_state.summarized_content = summarized_content
         st.write(st.session_state.summarized_content)
 
-    st.session_state.prompt = st.text_area("리드문을 대략 써서 넣으세요.", st.session_state.prompt, height=300)
+    prompt = st.text_area("리드문을 대략 써서 넣으세요.", height=300)
 
     if st.button("생성하기"):
-        if len(st.session_state.prompt) <= 10:
-            st.markdown("<span style='color:red'>10자 이상의 리드문을 입력해주세요.</span>", unsafe_allow_html=True)
-            return
+        if len(prompt) <= 10:
+            st.markdown("<span style='color:red'>10자 이상 써 주세요.</span>", unsafe_allow_html=True)
+        else:
+            with st.spinner('GPT-4가 기사로 만들고 있어요. 잠시 기다려주세요!'):
+                article_content = fetch_from_openai("gpt-4", [
+                    {"role": "user",
+                     "content": f"{st.session_state.summarized_content} 를 토대로 신문 기사를 쓸거야. 650자에서 1500자 내로 알아서 기사를 써 줘. 특히 숫자와 관련된 내용은 모두 나오도록 해 줘. 기사처럼 줄바꿈을 특히 잘 활용해. 앞서 작성한 리드문 '{prompt}'을 포함하도록 만들어줘."}
+                ], 'GPT-4가 기사로 만들고 있어요. 잠시 기다려주세요!')
 
-        final_article_content = st.session_state.prompt + "\n\n" + st.session_state.summarized_content
-        st.session_state.final_article_content = fetch_from_openai("gpt-4", [
-            {"role": "user",
-             "content": f"{summarized_content} 이 리포트 중에서 정보를 가져와 신문 기사를 써 줘. {st.session_state.prompt}에 써놓은 기사 시작문으로 문장을 시작해 줘. 참고하는 리포트 중에서 기사 시작문 부분 뒤쪽에 있는 정보들로만 기사를 만들어야 해. 정리된 리포트 내용과 조금 다른 문장 구조로 1000자 내로 기사를 써 줘. 특히 숫자와 관련된 내용을 다룰 때 틀리지마. '~한다' '~된다' '~했다' '~됐다'와 같은 반말로 써."}
-        ], "GPT4가 리포트를 기사 초안으로 만들고 있습니다.")
+                if article_content is None:
+                    return
+                st.write(article_content)
 
-    if st.session_state.final_article_content:
-        st.write("## 기사 초안")
-        st.write(st.session_state.final_article_content)
-
-    if st.session_state.final_article_content:
-        st.download_button(
-                        label="다운로드",
-                        data=st.session_state.final_article_content.encode("utf-8"),
-                        file_name="generated_article.txt",
-                        mime="text/plain",
-                    )
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
